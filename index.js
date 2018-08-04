0ef8f5743679f5a69120c286b4f57b39fe8b8087
Ambuj Gupta <ambuj.juet@gmail.com>
2018-08-04 17:06:44 +0530

    Settings List implemented

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var settingsButton = document.getElementsByClassName("fa-cog");
 var settingsList = document.getElementById("settingsList");
 
 document.addEventListener("click", function() {
-    //settingsList.style.display = "none";  
+    settingsList.style.display == "block" ? settingsList.style.display = "none" : null;
 });
 
-settingsButton[0].addEventListener("click", function() {
-    settingsList.style.display = "block";  
+settingsButton[0].addEventListener("click", function(event) {
+    event.stopPropagation();
+    settingsList.style.display == "block" ? settingsList.style.display = "none" : settingsList.style.display = "block";;
 });
 
 
